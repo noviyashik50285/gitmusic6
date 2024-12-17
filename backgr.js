3dd5a665bdaa7e2be6a1608d331aa644f5fd4955
noviyashik50285 <mynewdisk50285@gmail.com>
2024-12-17 15:23:08 +0300

    sec7 commit

--- backgr.js
+++ backgr.js
@@ -1,160 +1,183 @@
-const button0 = document.getElementById('butn0');
-button0.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone.jpg)';
-  obnulenie2();
-  this.style.color = 'blue';
-});
-const button1 = document.getElementById('butn1');
+const button1 = document.getElementById('butn01');
 button1.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone0.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim01").style.display = "block";
   this.style.color = 'blue';
 });
-const button2 = document.getElementById('butn2');
+const button2 = document.getElementById('butn02');
 button2.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone1.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim02").style.display = "block";
   this.style.color = 'blue';
 });
-const button3 = document.getElementById('butn3');
+const button3 = document.getElementById('butn03');
 button3.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone2.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim03").style.display = "block";
   this.style.color = 'blue';
 });
-const button4 = document.getElementById('butn4');
+const button4 = document.getElementById('butn04');
 button4.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone3.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim04").style.display = "block";
   this.style.color = 'blue';
 });
-const button5 = document.getElementById('butn5');
+const button5 = document.getElementById('butn05');
 button5.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone4.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim05").style.display = "block";
   this.style.color = 'blue';
 });
-const button6 = document.getElementById('butn6');
+const button6 = document.getElementById('butn06');
 button6.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone5.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim06").style.display = "block";
   this.style.color = 'blue';
 });
-const button7 = document.getElementById('butn7');
+const button7 = document.getElementById('butn07');
 button7.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone6.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim07").style.display = "block";
   this.style.color = 'blue';
 });
-const button8 = document.getElementById('butn8');
+const button8 = document.getElementById('butn08');
 button8.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone7.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim08").style.display = "block";
   this.style.color = 'blue';
 });
-const button9 = document.getElementById('butn9');
+const button9 = document.getElementById('butn09');
 button9.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone8.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim09").style.display = "block";
   this.style.color = 'blue';
 });
 const button10 = document.getElementById('butn10');
 button10.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone9.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim10").style.display = "block";
   this.style.color = 'blue';
 });
 const button11 = document.getElementById('butn11');
 button11.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone10.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim11").style.display = "block";
   this.style.color = 'blue';
 });
 const button12 = document.getElementById('butn12');
 button12.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone11.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim12").style.display = "block";
   this.style.color = 'blue';
 });
 const button13 = document.getElementById('butn13');
 button13.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone12.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim13").style.display = "block";
   this.style.color = 'blue';
 });
 const button14 = document.getElementById('butn14');
 button14.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone13.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim14").style.display = "block";
   this.style.color = 'blue';
 });
 const button15 = document.getElementById('butn15');
 button15.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone14.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim15").style.display = "block";
   this.style.color = 'blue';
 });
 const button16 = document.getElementById('butn16');
 button16.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone15.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim16").style.display = "block";
   this.style.color = 'blue';
 });
 const button17 = document.getElementById('butn17');
 button17.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone16.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim17").style.display = "block";
   this.style.color = 'blue';
 });
 const button18 = document.getElementById('butn18');
 button18.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone17.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim18").style.display = "block";
   this.style.color = 'blue';
 });
 const button19 = document.getElementById('butn19');
 button19.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone18.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim19").style.display = "block";
   this.style.color = 'blue';
 });
 const button20 = document.getElementById('butn20');
 button20.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone19.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim20").style.display = "block";
   this.style.color = 'blue';
 });
 const button21 = document.getElementById('butn21');
 button21.addEventListener('click', function() {
-  const body = document.querySelector('body');
-  body.style.backgroundImage = 'url(back/yellowstone20.jpg)';
+  obnuleniefon();
   obnulenie2();
+  document.getElementById("prim21").style.display = "block";
+  this.style.color = 'blue';
+});
+const button22 = document.getElementById('butn22');
+button22.addEventListener('click', function() {
+  obnuleniefon();
+  obnulenie2();
+  document.getElementById("prim22").style.display = "block";
   this.style.color = 'blue';
 });
 
+var obnuleniefon = function(){
+  document.getElementById("prim01").style.display = "none";  
+  document.getElementById("prim02").style.display = "none";  
+  document.getElementById("prim03").style.display = "none";  
+  document.getElementById("prim04").style.display = "none";  
+  document.getElementById("prim05").style.display = "none";  
+  document.getElementById("prim06").style.display = "none";  
+  document.getElementById("prim07").style.display = "none";  
+  document.getElementById("prim08").style.display = "none";  
+  document.getElementById("prim09").style.display = "none";  
+  document.getElementById("prim10").style.display = "none";  
+  document.getElementById("prim11").style.display = "none";  
+  document.getElementById("prim12").style.display = "none";  
+  document.getElementById("prim13").style.display = "none";  
+  document.getElementById("prim14").style.display = "none";  
+  document.getElementById("prim15").style.display = "none";  
+  document.getElementById("prim16").style.display = "none";  
+  document.getElementById("prim17").style.display = "none";  
+  document.getElementById("prim18").style.display = "none";  
+  document.getElementById("prim19").style.display = "none";  
+  document.getElementById("prim20").style.display = "none";  
+  document.getElementById("prim21").style.display = "none";  
+  document.getElementById("prim22").style.display = "none";  
+}
 var obnulenie2 = function(){
-  button0.style.color = 'red';
   button1.style.color = 'red';
   button2.style.color = 'red';
   button3.style.color = 'red';
@@ -176,5 +199,6 @@ var obnulenie2 = function(){
   button19.style.color = 'red';
   button20.style.color = 'red';
   button21.style.color = 'red';
+  button22.style.color = 'red';
 }
 
